aa596b0eecbd3fd7475605d0c522fa63e1ba1571
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-06-26 15:06:50 -0300

    03-07 - Configurando Sequelize

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,7 @@
-import  {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>{
-  return res.json({message: 'Hello Kingston Brabo!'});
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello Kingston Brabo!' }));
 
 export default routes;
