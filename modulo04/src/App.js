1a7e8841881b1f7a2da73b523660fec45685daeb
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-09-04 15:07:16 -0300

    Class components

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/dava.jpg';
-
+import TechList from './components/TechList';
 
 function App(){
-  return <img width="200" src={profile} alt=""/>
+  return <TechList />
 
 }
 
