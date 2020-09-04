855eff1e1f8307a967c90be5c02c4400b1b8edd4
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-09-04 15:34:52 -0300

    Estado & Imutabilidade

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 class TechList extends Component{
 
   state={
+    newTech: '',
     techs: [
       'Node.js',
       'ReactJS',
@@ -10,14 +11,34 @@ class TechList extends Component{
     ]
   };
 
+  handleInputChange = e =>{
+    this.setState({newTech: e.target.value});
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+
+    this.setState({ 
+      techs: [... this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  }
 
   render(){
     return (
+      
+      <form onSubmit={this.handleSubmit}>
       <ul>
-        <li>Node.JS</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
+        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
+      <input 
+        type="text" 
+        onChange={this.handleInputChange} 
+        value={this.state.newTech}
+      />
+      <button type="submit">Submit</button>
+      </form>
+  
     );
   }
 }
