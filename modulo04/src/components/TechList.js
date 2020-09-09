450711952b183caf72c966cbc3014851eba07ff4
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-09-09 13:44:02 -0300

    Propriedades do React

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -1,53 +1,47 @@
-import React, {Component} from 'react';
-
-class TechList extends Component{
-
-  state={
-    newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+import React, { Component } from "react";
+import TechItem from "./TechItem";
+class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: ["Node.js", "ReactJS", "React Native"],
   };
 
-  handleInputChange = e =>{
-    this.setState({newTech: e.target.value});
-  }
+  handleInputChange = (e) => {
+    this.setState({ newTech: e.target.value });
+  };
 
-  handleSubmit = e =>{
+  handleSubmit = (e) => {
     e.preventDefault();
 
-    this.setState({ 
-      techs: [... this.state.techs, this.state.newTech],
-      newTech: '',
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: "",
     });
-  }
+  };
 
-  handleDelete = (tech) =>{
-    this.setState({ techs: this.state.techs.filter(t => t !== tech)})
-  }
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter((t) => t !== tech) });
+  };
 
-  render(){
+  render() {
     return (
-      
       <form onSubmit={this.handleSubmit}>
-      <ul>
-        {this.state.techs.map(tech => (
-          <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
-            </li>
-            ))}
-      </ul>
-      <input 
-        type="text" 
-        onChange={this.handleInputChange} 
-        value={this.state.newTech}
-      />
-      <button type="submit">Submit</button>
+        <ul>
+          {this.state.techs.map((tech) => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Submit</button>
       </form>
-  
     );
   }
 }
