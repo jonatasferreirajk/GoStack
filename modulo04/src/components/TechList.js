f662f25c67d20c9c0b58ff451ffa3e3ae8771736
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-09-11 13:28:38 -0300

    Default Props & PropTypes

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -34,6 +34,7 @@ class TechList extends Component {
               onDelete={() => this.handleDelete(tech)}
             />
           ))}
+          <TechItem />
         </ul>
         <input
           type="text"
